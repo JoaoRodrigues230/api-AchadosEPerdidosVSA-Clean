ef7eb88dd29754a17c5fd285838ef8483057ac72
Joao Vitor Pereira Rodrigues <joaovp.rodrigues@outlook.com>
2026-05-31 12:59:02 -0300

    feat: adicionando rota de listagem e parametros de filtro.

--- src/Features/Itens/ItemController.cs
+++ src/Features/Itens/ItemController.cs
@@ -1,7 +1,8 @@
-using API_AchadosEPerdidos.Features.Itens.CreateItem;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using API_AchadosEPerdidos.Features.Itens.CreateItem;
+using API_AchadosEPerdidos.Features.Itens.BuscarItensHandler;
 
 namespace API_AchadosEPerdidos.Controllers;
 
@@ -22,6 +23,14 @@ public class ItemController : ControllerBase
     {
         var resultado = await _mediator.Send(command);
         
+        await resultado.ExecuteAsync(HttpContext);
+        return new EmptyResult();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> BuscarItens([FromQuery] BuscarItensQuery query)
+    {
+        var resultado = await _mediator.Send(query);
         await resultado.ExecuteAsync(HttpContext);
         return new EmptyResult();
     }
